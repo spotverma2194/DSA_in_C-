f67e4dc912abe08ef82f7d8dd10ff2ae87d8a7d9
deepak <deepakverma0522@gmail.com>
2024-12-21 11:13:11 +0530

    Printing array

---
+++ 06_Array/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int aar[5];
+    aar[0]=9;
+    aar[1]=3;
+    aar[2]=2;
+    aar[3]=5;
+    aar[4]=6;
+    for(int i=0;i<5;i++){
+        cout<<aar[i]<<" ";
+    }
+    return 0;
+
+}
