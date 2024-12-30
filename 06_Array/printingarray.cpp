a081af8f2845952a852756eff978f28eb77fbaae
deepak <deepakverma0522@gmail.com>
2024-12-30 16:58:27 +0530

    Printing arrray n times

---
+++ 06_Array/printingarray.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,3,13,8,9};
+    for(int i=1;i<=5;i++){
+        for(int j=0;j<5;j++){
+            cout<<arr[j]<<" ";
+        }
+        cout<<endl;
+    }
+}
