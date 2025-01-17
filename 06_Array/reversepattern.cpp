9713eb82e602fe35a039b4ac24dad85b7534808f
deepak <deepakverma0522@gmail.com>
2025-01-17 14:34:11 +0530

    Some changes

--- 06_Array/reversepattern.cpp
+++ 06_Array/reversepattern.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arrr[5]={3,13,12,8,4};
     for(int i=0;i<5;i++){
-        for(int j=4-i;j>=0;j--){
+        for(int j=4;j>=i;j--){
             cout<<arrr[j]<<" ";
         }
         cout<<endl;
